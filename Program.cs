f5bbe63512da666333758b78bb8ac86576b3e308
KusumaNaiduB <kusumanaidu8@gmail.com>
2024-02-16 09:34:22 +0530

    'AllFiles'

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.Globalization;
+
+Console.WriteLine("Welcome to Number System");
+Console.WriteLine("*** Even Numbers***");
+NumberSystem.EvenNum();
+NumberSystem.EvenNum();
+NumberSystem.OddNum();
